bc168b23e7c2f5b4588bae6c18c5b9054cf05f39
Nikita Duritsyn <kodtochka32@gmail.com>
2023-02-25 19:30:15 +0300

    [~] getVisitorsBySession

--- controllers/visitorsController.js
+++ controllers/visitorsController.js
@@ -2,26 +2,26 @@ import { pool } from '../db.js'
 
 export class visitorsController {
     async createVisitorNotSession(req, res) {
-        try {
-            console.log(req.body);
-            const { tariff_id, name, lastname, number_phone, deposit, deponent, status } = req.body
-            const visitor = await pool.query(`INSERT INTO visitors ( tariff_id, name, lastname, number_phone, deposit, deponent, status) values ($1, $2, $3, $4, $5, $6, $7) RETURNING *`, [tariff_id, name, lastname, number_phone, deposit, deponent, status])
-            res.json(visitor.rows[0])
-        } catch (e) {
-            console.log('Ошибка ' + e.name + ":\n " + e.message + "\n\n" + e.stack);
-        }
+        // try {
+        //     console.log(req.body);
+        //     const { tariff_id, name, lastname, number_phone, deposit, deponent, status } = req.body
+        //     const visitor = await pool.query(`INSERT INTO visitors ( tariff_id, name, lastname, number_phone, deposit, deponent, status) values ($1, $2, $3, $4, $5, $6, $7) RETURNING *`, [tariff_id, name, lastname, number_phone, deposit, deponent, status])
+        //     res.json(visitor.rows[0])
+        // } catch (e) {
+        //     console.log('Ошибка ' + e.name + ":\n " + e.message + "\n\n" + e.stack);
+        // }
     }
     async createVisitor(req, res) {
-        try {
-            // НУЖНО УТОЧНИТЬ МОМЕНТ КСТАТИ МОЖЕТ ЛИ ВООБЩЕ ТАКОЕ БЫТЬ ЧТО ДЕПОНЕНТ НЕ ВНЕСЛИ ПРИ БРОНИ
-            // Тут еще может быть депонент в Payment, при условии что он есть
-            const sessionId = req.params.id
-            const { tariff_id, name, lastname, number_phone, deposit, deponent, status } = req.body
-            const visitor = await pool.query(`INSERT INTO visitors (session_id, tariff_id, name, lastname, number_phone, deposit, deponent, status) values ($1, $2, $3, $4, $5, $6, $7, $8) RETURNING *`, [sessionId, tariff_id, name, lastname, number_phone, deposit, deponent, status])
-            res.json(visitor.rows[0])
-        } catch (e) {
-            console.log('Ошибка ' + e.name + ":\n " + e.message + "\n\n" + e.stack);
-        }
+        // try {
+        //     // НУЖНО УТОЧНИТЬ МОМЕНТ КСТАТИ МОЖЕТ ЛИ ВООБЩЕ ТАКОЕ БЫТЬ ЧТО ДЕПОНЕНТ НЕ ВНЕСЛИ ПРИ БРОНИ
+        //     // Тут еще может быть депонент в Payment, при условии что он есть
+        //     const sessionId = req.params.id
+        //     const { tariff_id, name, lastname, number_phone, deposit, deponent, status } = req.body
+        //     const visitor = await pool.query(`INSERT INTO visitors (session_id, tariff_id, name, lastname, number_phone, deposit, deponent, status) values ($1, $2, $3, $4, $5, $6, $7, $8) RETURNING *`, [sessionId, tariff_id, name, lastname, number_phone, deposit, deponent, status])
+        //     res.json(visitor.rows[0])
+        // } catch (e) {
+        //     console.log('Ошибка ' + e.name + ":\n " + e.message + "\n\n" + e.stack);
+        // }
     }
     async getAllVisitors(req, res) {
         try {
@@ -34,8 +34,7 @@ export class visitorsController {
     async getAllVisitorsSession(req, res) {
         try {
             const sessionId = req.params.id
-            const visitors_sessions_durations = await pool.query(`SELECT * FROM visitors_sessions_durations where session_id = $1`, [sessionId])
-            const visitors = await pool.query(`SELECT * FROM visitors where visitor_session_duration_id = $1`, [visitors_sessions_durations.rows[0].id])
+            const visitors = await pool.query(`SELECT * FROM visitors where session_id = $1`, [sessionId])
             res.json(visitors.rows)
         } catch (e) {
             console.log('Ошибка ' + e.name + ":\n " + e.message + "\n\n" + e.stack);
