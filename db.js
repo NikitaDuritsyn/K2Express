839a4742918657f24d2ee17b04237acd18094666
unknown <duritsyn.nikita@hubbiton.info>
2023-03-09 16:18:06 +0300

    [~] DataBase

--- db.js
+++ db.js
@@ -3,7 +3,7 @@ const { Pool } = pkg;
 
 export const pool = new Pool({
     user: "NikitaDuritsyn",
-    password: "123123",
+    password: "zxc",
     host: "localhost",
     port: 5432,
     database: "kamenka2",
